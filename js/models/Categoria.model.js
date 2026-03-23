05cf715cae74f329bba4ae566991c79fdd103513
Caio Rosa - Coordenador <Desenvolvimento@ipolisboa.min-saude.pt>
2026-03-23 11:46:29 +0000

    feat: configuracao inicial

---
+++ js/models/Categoria.model.js
@@ -0,0 +1,5 @@
+// #id;
+// #nome;
+// #descricao;
+// #ativo;
+// #dataCriacao;
---
+++ js/models/UserDemo.model.js
@@ -0,0 +1,110 @@
+export class UserDemo {
+  // propriedades
+  #id;
+  #numMec;
+  #nome;
+  #email;
+  #categoriaId;
+  #ativo;
+  #dataInicioFuncoes;
+  #dataFimFuncoes;
+  #dataCriacao;
+  #criadoPor;
+
+  constructor(data = {}) {
+    this.#id = data.id ?? null;
+    this.#numMec = data.numMec ?? null;
+    this.#nome = data.nome ?? "";
+    this.#email = data.email ?? "";
+    this.#categoriaId = data.categoriaId ?? 1;
+    this.#ativo = data.ativo ?? true;
+    this.#dataInicioFuncoes = data.dataInicioFuncoes
+      ? new Date(data.dataInicioFuncoes)
+      : null;
+    this.#dataFimFuncoes = data.dataFimFuncoes
+      ? new Date(data.dataFimFuncoes)
+      : null;
+    this.#dataCriacao = data.dataCriacao
+      ? new Date(data.dataCriacao)
+      : new Date();
+    this.#criadoPor = data.criadoPor ?? "";
+  }
+
+  // GETS
+  get id() {
+    return this.#id;
+  }
+  get numMec() {
+    return this.#numMec;
+  }
+  get nome() {
+    return this.#nome;
+  }
+  get email() {
+    return this.#email;
+  }
+  get categoriaId() {
+    return this.#categoriaId;
+  }
+  get ativo() {
+    return this.#ativo;
+  }
+  get dataInicioFuncoes() {
+    return this.#dataInicioFuncoes;
+  }
+  get dataFimFuncoes() {
+    return this.#dataFimFuncoes;
+  }
+  get dataCriacao() {
+    return this.#dataCriacao;
+  }
+  get criadoPor() {
+    return this.#criadoPor;
+  }
+
+  get nomeAbreviado() {
+    if (!this.#nome) return "-";
+    // caio roberto rosa
+    const partes = this.#nome.trim().split(" ");
+    return partes.length > 1 ? `${partes[partes.length - 1]}` : partes[0];
+  }
+
+  // SETTERS
+  set email(value) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(value)) throw new Error("Email inválido.");
+
+    this.#email = value.toLowerCase().trim();
+  }
+
+  toApiPayload(includeId = true) {
+    const payload = {
+      numMec: this.#numMec,
+      nome: this.#nome,
+      email: this.#email,
+      categoriaId: this.#categoriaId,
+      ativo: this.#ativo,
+      dataInicioFuncoes: this.#dataInicioFuncoes?.toISOString() ?? null,
+      dataFimFuncoes: this.#dataFimFuncoes?.toISOString() ?? null,
+      criadoPor: this.#criadoPor,
+    };
+
+    if (includeId && this.#id) payload.id = this.#id;
+
+    return payload;
+  }
+
+  // FACTORY
+  static fromApi(raw) {
+    return new UserDemo(raw);
+  }
+  static fromApiList(arr) {
+    return Array.isArray(arr) ? arr.map(UserDemo.fromApi) : [];
+  }
+
+
+  toString(){
+    return `User(id=${this.#id}, numMec= ${this.#numMec}, nome="${this.#nome}")`;
+  }
+
+}
